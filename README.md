11e85ec68941ed1d49bc7403aa8949057e2c6002
Jungwoon Shin <jungwoonshin@gmail.com>
2020-05-18 19:13:48 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ICTC
+ICTC is a bipartite link prediction method.\
+Note: to run this model, please use python version above 3.0.
+
+To run the model, please first select one dataset from args.py while commenting out the ones that you are not using.\
+You have three model choices. 
+
+To run ICTC, python train.py. This will run GAE,LGAE,ICTC.\
+To run BiSPM, python bispm.py \
+To run SRNMF, choose one similarity measure which is one of [CN, JC,CPA] by selecting one from srnmf.py. (i.e. args.similarity = 'srnmf_cn') then python srnmf.py
+
+
---
+++ args.py
@@ -0,0 +1,40 @@
+### CONFIGS ###
+
+# dataset = 'gpcr'
+# dataset='enzyme'
+# dataset = 'ionchannel'
+# dataset= 'malaria'
+dataset = 'drug'
+# dataset = 'sw'
+# dataset = 'nanet'
+# dataset = 'movie100k'
+
+model1 = 'LGAE'
+model2 = 'GAE' 
+
+# input_dim1 = 318 # gpcr bipartite
+# input_dim1 = 1109 # enzyme bipartite
+# input_dim1 = 414 # ion channel bip
+# input_dim1 = 1103 # malaria
+# input_dim1 = 350 # drug
+# input_dim1 = 32 # southernwomen
+# input_dim1 =1880 # nanet
+# input_dim1 = 2625 # movie100k
+
+device = 1
+
+learning_rate = 0.01
+num_epoch = 200
+
+print_val = False
+use_saved_edge_false = False
+save_edge_false = False
+
+hidden1_dim = 32
+hidden2_dim = 16
+
+numexp = 10
+num_test = 10./1. # 10/1 means 10% means 10% is used as test sets., 10/2 means 20% is used as test sets. 
+
+weight_seed = 100
+edge_idx_seed = 100
